30cb15c9d5dbf2ecfe7e5bc634c4fb51a01a2e92
Kleanton1 <Kleanton1>
2023-03-02 01:48:01 +0100

    basic components

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,9 @@
+import { shallow } from "enzyme";
+import App from './App';
+
+describe('<App />', () => {
+  it('Tests that App renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  })
+});
