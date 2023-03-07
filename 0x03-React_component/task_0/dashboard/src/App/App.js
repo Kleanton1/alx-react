27b0d578c47e37c18a9b406ee127c0a0ccc85aea
Kleanton1 <Kleanton1>
2023-03-07 21:42:05 +0100

    class component

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React, from "react";
+import React from "react";
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Login from "../Login/Login";
@@ -8,21 +8,19 @@ import propTypes from "prop-types";
 import { getLatestNotification } from "../utils/utils";
 import "./App.css";
 
-// implement class components
-class App extends React.Component {
-    listCourses = [
+    const listCourses = [
       { id: 1, name: "ES6", credit: "60" },
       { id: 2, name: "Webpack", credit: '20' },
       { id: 3, name: "React", credit: "40" }
     ];
 
-    listNotifications = [
+    const listNotifications = [
       { id: 1, type: "default", value: "New course available"},
       { id: 2, type: "urgent", value: "New resume available" },
       { id: 3, type: "urgent", html: getLatestNotification() },
     ];
 
-    render() {
+    function App(isLoggedIn) {
     return (
       <React.Fragment>
       <div className="App">
@@ -30,13 +28,12 @@ class App extends React.Component {
         <Notifications listNotifications={listNotifications} />
         <Header />
         </div>
-        {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
           <Footer />
         </div>
         </React.Fragment>
-    )
+    );
   }
-}
 
 App.defaultProps = {
   isLoggedIn: false,
